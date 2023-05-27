3419b326b23785b216b525ec49cf98f0e58714b3
Facundo Zapata <facundozapata41@gmail.com>
2023-05-27 19:36:09 -0300

    avances

--- e-commerce/back-end/app.js
+++ e-commerce/back-end/app.js
@@ -1,12 +1,26 @@
 import express from 'express'
 import routerMaster from './routes/index.js'
 import connection from './connection/connection.js'
+import User from './Models/User.js'
 const app = express();
 
-//middleware
+// const rickAndMortyAPI = fetch("https://rickandmortyapi.com/api/episode?page=3")
+//                             .then((response)=>response.json())
+//                             .then((data)=>console.log(data))
 
+//---------------- middleware --------------------//
+app.use(express.urlencoded({ extended : false}))
 app.use(routerMaster)
 
-app.listen(8081,()=>{
-    console.log("http://localhost:8081")
+//-------------- sincronización con la BD -------------//
+// tengo que importar en este archivo las tablas que voy a tocar 
+// connection.sync() esta forma de sincronizar no mira cambios, si las tablas existen no hace nada. 
+// connection.sync({ force: true }) tira todas las tablas de mi base de datos y las vuelve a crear segun modelos 
+// connection.sync({ alter: true }) mira todas las tablas y comprueba cambios, de encontrar cambios los actualiza sin tirar la tabla
+
+await connection.sync({ force:false }).then(() => {
+    app.listen(8081,()=>{
+        console.log("http://localhost:8081")
+    })
 })
+
--- e-commerce/back-end/routes/userRoutes.js
+++ e-commerce/back-end/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import User from './../Models/User.js'
 const userRoutes = Router();
 
 userRoutes.get("/",(req,res)=>{
@@ -9,7 +10,30 @@ userRoutes.get("/:id",(req,res)=>{
     res.send("get con ID!!")
 })
 
+userRoutes.post("/create",(req,res)=>{
+    const { name, lastName, password, email} = req.body
+    // esto era si uso mysql2 derecho en mi connection pero aca ya tengo sequelize! 
+    // connection.query(
+    //     `INSERT INTO USERS (name,lastName,password,email) VALUES(${name},${lastName},${password},${email})`
+    // ).then(()=>{
+    //     result = "Usuario creado exitosamente!"
+    // }).catch(()=>{
+    //     result = "error en base de datos"
+    // })
+
+    const newUser = User.create({
+        name: "Facundo",
+        lastName:"Zapata",
+        password:"1234",
+        email:"fzapata@gmail.com" 
+    }, {fields: [name,lastName,password,email]
+    })
+    
+    res.send("Todo ok")
+})
+
 userRoutes.post("/:id",(req,res)=>{
+
     res.send("post con ID!!")
 })
 
